2fa3717e11bb43f710374ca39a5d4ddff2a6e07a
Rayhan Shorker <raihanshorker56@gmail.com>
2023-06-28 02:48:24 +0600

    added footer

--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -2,9 +2,11 @@
 
 const Footer = () => {
     return (
-        <div>
-            <h1 className="text3xl">Footer</h1>
-        </div>
+        <footer className="footer text-white font-mono footer-center p-4 bg-black mt-12">
+  <div>
+    <p>Copyright &copy; {new Date().getFullYear()} - All right reserved by Rayhan Shorker</p>
+  </div>
+</footer>
     );
 };
 
